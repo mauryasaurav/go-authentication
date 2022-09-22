12b55d33098c40e679fd264a983b45b3090147d0
mauryasaurav <s2maurya1@gmail.com>
2022-09-22 16:33:02 +0530

    CI/CD

--- utils/constants/constants.go
+++ utils/constants/constants.go
@@ -1,4 +1,4 @@
 package constants
 
-var DB_URL string = "postgres://root:password@localhost:5432/power"
+var DB_URL string = "postgres://postgres:testing123@localhost:5432/go-auth"
 var SECRET_KEY string = "rbg98HWKQHo4SYfS/5m+KQ+eL9t95lr25ASurmojE2/jAnNGnB2bUg+HBsQzvBOl+cQTTM3ZOkWteFrZ"

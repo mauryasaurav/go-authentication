8e16f10ab9157e89affcdafab5da2f70173ebcb1
mauryasaurav <s2maurya1@gmail.com>
2022-09-22 17:27:20 +0530

    CI/CD

--- utils/constants/constants.go
+++ utils/constants/constants.go
@@ -1,4 +1,4 @@
 package constants
 
-var DB_URL string = "postgres://postgres:testing123@localhost:5432/go-auth"
+var DB_URL string = "postgres://root:password@postgresql-88866-0.cloudclusters.net:13015/go-auth"
 var SECRET_KEY string = "rbg98HWKQHo4SYfS/5m+KQ+eL9t95lr25ASurmojE2/jAnNGnB2bUg+HBsQzvBOl+cQTTM3ZOkWteFrZ"
